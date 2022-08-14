e5749d71be93e4eeb06b8ccea18d2f48e4d7b960
Karim DRIDI <seth.holth@gmail.com>
2022-08-14 22:02:00 +0000

    C : problems/TEST

---
+++ problems/TEST/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(void) {
+    int value;
+
+    while (true) {
+        fscanf(stdin, "%d\n", &value);
+        if (value == 42)
+            break;
+        printf("%d\n", value);
+    }
+
+    exit(EXIT_SUCCESS);
+}
